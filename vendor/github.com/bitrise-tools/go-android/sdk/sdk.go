41cde2c9d82e78b70163e6f21dade8e63e47c492
Mohammad Jafarzadeh Rezvan <mohammad.rezvan@virta.global>
2020-10-19 17:12:02 +0300

    revert the change: test

--- vendor/github.com/bitrise-tools/go-android/sdk/sdk.go
+++ vendor/github.com/bitrise-tools/go-android/sdk/sdk.go
@@ -44,8 +44,31 @@ func (model *Model) GetAndroidHome() string {
 // LatestBuildToolsDir ...
 func (model *Model) LatestBuildToolsDir() (string, error) {
 	buildTools := filepath.Join(model.androidHome, "build-tools")
+	pattern := filepath.Join(buildTools, "*")
 
-	return filepath.Join(buildTools, "29.0.2"), nil
+	buildToolsDirs, err := filepath.Glob(pattern)
+	if err != nil {
+		return "", err
+	}
+
+	var latestVersion *version.Version
+	for _, buildToolsDir := range buildToolsDirs {
+		versionStr := strings.TrimPrefix(buildToolsDir, buildTools+"/")
+		version, err := version.NewVersion(versionStr)
+		if err != nil {
+			continue
+		}
+
+		if latestVersion == nil || version.GreaterThan(latestVersion) {
+			latestVersion = version
+		}
+	}
+
+	if latestVersion.String() == "" {
+		return "", errors.New("failed to find latest build-tools dir")
+	}
+
+	return filepath.Join(buildTools, latestVersion.String()), nil
 }
 
 // LatestBuildToolPath ...
